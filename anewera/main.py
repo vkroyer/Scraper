0cfe7d7820e767c1a0b9af06f75afb6a97d5fbf1
vkroyer <vebjorn.kroyer@gmail.com>
2023-06-20 19:42:50 +0200

    testing sending mail

--- anewera/main.py
+++ anewera/main.py
@@ -6,6 +6,19 @@ from projects import instantiate_person, AllProjects
 from scrape import Scraper
 from user import UserPreferences
 
+
+def update_json_files(all_projects:AllProjects):
+    person_json_content = json.dumps({person.name:person.__dict__ for person in all_projects.persons}, indent=4)
+    film_project_json_content = json.dumps({project.id:project.__dict__ for project in all_projects.film_projects}, indent=4)
+
+    with open("data/person_log.json", "w") as f:
+        f.write(person_json_content)
+
+    with open("data/film_project_log.json", "w") as f:
+        f.write(film_project_json_content)
+
+
+
 def main():
     all_projects = AllProjects()
     all_projects.get_previous_persons()
@@ -17,7 +30,7 @@ def main():
 
     with requests.Session() as session:
         scraper = Scraper(session=session)
-        
+
         for director_name in user.directors:
             if director_name in [person.name for person in all_projects.directors]:
                 continue
@@ -30,23 +43,24 @@ def main():
             actor = instantiate_person(scraper=scraper, name=actor_name, is_director=False, is_actor=True)
             all_projects.add_person(actor)
 
+        # TODO: Remove projects that have been released
+        # Scrape new upcoming projects from all persons
         for person in all_projects.persons:
             projects = scraper.get_projects(person)
-            all_projects.add_projects(projects=projects)
+            new_projects = []
             for project in projects:
+                if project.id in person.projects:
+                    continue
+                new_projects.append(project)
                 person.projects.append(project.id)
 
+            all_projects.add_projects(projects=new_projects)
 
-    person_json_content = json.dumps({person.name:person.__dict__ for person in all_projects.persons}, indent=4)
-    film_project_json_content = json.dumps({project.id:project.__dict__ for project in all_projects.film_projects}, indent=4)
+    mail_content_markdown = format_mail(all_projects=all_projects, actor_flag=False)
+    mail_content_html = markdown(mail_content_markdown)
 
-    with open("data/person_log.json", "w") as f:
-        f.write(person_json_content)
-
-    with open("data/film_project_log.json", "w") as f:
-        f.write(film_project_json_content)
-
-
+    subject = "Roundup of Upcoming Movies and TV Shows"
+    send_email(subject=subject, content=mail_content_html)
     
 
 if __name__ == "__main__":
